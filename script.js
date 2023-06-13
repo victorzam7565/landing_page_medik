0ffbdea9347301fadc110f40d4aa5f009f115c46
victor adrian zambrano tamayo <ingenieriainversazambrano@gmail.com>
2023-06-13 18:40:57 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper",{ 
+slidesPerView : 1,
+centeredSlides:true,
+loop:true,
+spaceBetween:30,
+grabCursor:true,
+navigation:{
+    nextEl:'.swiper-button-next',
+},
+breakpoints:{
+    991:{
+        slidesPerView:1 
+    }
+}
+});
+
+
